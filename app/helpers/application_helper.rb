78dd93804f05da0b18a8ab05077ca36628dd242d
AR-H <alireza.hokmabadi1989@gmail.com>
2013-06-09 12:52:28 +0430

    complete tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  def tag_cloud(tags, classes)
+    max = tags.sort_by(&:count).last
+    tags.each do |tag|
+      index = tag.count.to_f / max.count * (classes.size - 1)
+      yield(tag, classes[index.round])
+    end
+  end
 end
