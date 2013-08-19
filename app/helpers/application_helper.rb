b5db9e289419b8fee4ce90e73419a7de4925393b
AR-H <alireza.hokmabadi1989@gmail.com>
2013-08-19 14:10:36 +0430

    add jalalidate to app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,16 @@ module ApplicationHelper
     end
   end
   
+  def to_jalali(date, options = {})
+    jalali_date = JalaliDate.new(date)	
+    if options[:standard]
+      jalali_date.strftime("%e %b %Y").to_farsi
+    elsif options[:just_time]
+      "ساعت " + jalali_date.strftime("%H:%M").to_farsi
+    else
+      jalali_date.strftime("%A %e %b %Y - %H:%M").to_farsi
+    end
+  end
+  
   
 end
