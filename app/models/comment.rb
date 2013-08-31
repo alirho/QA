007f87863eccd5f32794727a14cd5e58a2b68650
AR-H <alireza.hokmabadi1989@gmail.com>
2013-08-31 12:05:09 +0430

    bugfix link answer and comment user profile

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  validates :body, presence: true, length: { minimum: 20 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
